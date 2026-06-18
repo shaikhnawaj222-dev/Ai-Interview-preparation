2c07e431b6d1762bc9cd15ab7117af07faecadd7
Nawaz Shaikh <nawajshaikh031@gmail.comgit config --global user.email nawajshaikh031@gmail.comgit config --global user.email nawajshaikh031@gmail.com>
2026-06-18 22:54:11 +0530

    Fix CORS

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://ai-interview-preparation-fl6e.onrender.com"),
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    (import.meta.env.MODE === "development"
+      ? "http://localhost:3000"
+      : "https://ai-interview-preparation-fl6e.onrender.com"),
   withCredentials: true,
 });
 
