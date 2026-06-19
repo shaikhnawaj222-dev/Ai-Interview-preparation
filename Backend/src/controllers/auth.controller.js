208d1157a2cbfe28d982d84aa4b08472f97e4c5a
Nawaz Shaikh <nawajshaikh031@gmail.comgit config --global user.email nawajshaikh031@gmail.comgit config --global user.email nawajshaikh031@gmail.com>
2026-06-19 17:08:02 +0530

    auth setting redesign

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -41,7 +41,11 @@ async function registerUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 
   res.status(201).json({
     message: "User registered successfully",
@@ -83,7 +87,11 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
   res.status(200).json({
     message: "User loggedIn successfully.",
     user: {
@@ -106,7 +114,11 @@ async function logoutUserController(req, res) {
     await tokenBlacklistModel.create({ token });
   }
 
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 
   res.status(200).json({
     message: "User logged out successfully",
